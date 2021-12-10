53ef5c689750500602d67b26dc14b8726604b0fd
Pablo <ppereiramoure@danielcastelao.org>
2021-12-10 12:47:06 +0100

    Loop terminado

--- html/wp-content/themes/newfancy/index.php
+++ html/wp-content/themes/newfancy/index.php
@@ -8,8 +8,7 @@
 
  <body>
 
- <?php get_header() ?>
-
+ <?php define( 'WP_USE_THEMES', false ); get_header(); ?>
  <div id="cuerpo">
      <?php
 
@@ -18,6 +17,22 @@
      ?>
 
  </div>
+
+ <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+     <div class="entry">
+         <div class="imagen"><?php the_post_thumbnail([100,100])?></div>
+         <div class="contenido">
+             <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+             <small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small>
+             <?php //the_content();
+             the_excerpt();
+             ;?>
+             <p class="postmetadata"><?php _e( 'Posted in' ); ?> <?php the_category( ', ' ); ?></p>
+         </div>
+     </div> <!-- closes the first div box -->
+ <?php endwhile; else : ?>
+     <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+ <?php endif; ?>
 
  <?php get_footer() ?>
 
